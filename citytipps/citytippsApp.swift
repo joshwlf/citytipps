9e4c4a6fe09a3949a4a49188ce873b85aab063a7
joshwlf <joshiwolfram@googlemail.com>
2023-03-06 17:54:16 +0100

    Initial Commit

---
+++ citytipps/citytippsApp.swift
@@ -0,0 +1,17 @@
+//
+//  citytippsApp.swift
+//  citytipps
+//
+//  Created by Joshi Wolfram on 06.03.23.
+//
+
+import SwiftUI
+
+@main
+struct citytippsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
